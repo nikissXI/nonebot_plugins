1fca001c7ad5ff881bf07f98e51f756cf5cc6f8d
nikissXI <1299577815@qq.com>
2023-01-16 22:23:39 +0800

    0.1.3

--- nonebot_plugin_easy_translate/nonebot_plugin_easy_translate/__init__.py
+++ nonebot_plugin_easy_translate/nonebot_plugin_easy_translate/__init__.py
@@ -1,5 +1,5 @@
 from httpx import AsyncClient
-from nonebot import get_bot, get_bots, get_driver, on_regex
+from nonebot import on_regex
 from nonebot.adapters import Bot
 from nonebot.adapters.onebot.v11.event import (
     GroupMessageEvent,
@@ -8,7 +8,7 @@ from nonebot.adapters.onebot.v11.event import (
 )
 from nonebot.params import RegexGroup
 from nonebot.plugin import PluginMetadata
-from .config import plugin_config as pc
+from .config import pc, var
 
 __plugin_meta__ = PluginMetadata(
     name="简单翻译插件",
@@ -18,83 +18,12 @@ __plugin_meta__ = PluginMetadata(
 """,
 )
 
-handle_bot: None | Bot = None
-
-
-driver = get_driver()
-
-# qq机器人连接时执行
-@driver.on_bot_connect
-async def on_bot_connect(bot: Bot):
-    global handle_bot
-    # 是否有写bot qq，如果写了只处理bot qq在列表里的
-    if (
-        pc.easy_translate_bot_qqnum_list
-        and bot.self_id in pc.easy_translate_bot_qqnum_list
-    ):
-        # 如果已经有bot连了
-        if handle_bot:
-            # 当前bot qq 下标
-            handle_bot_id_index = pc.easy_translate_bot_qqnum_list.index(
-                handle_bot.self_id
-            )
-            # 连过俩的bot qq 下标
-            new_bot_id_index = pc.easy_translate_bot_qqnum_list.index(bot.self_id)
-            # 判断优先级，下标越低优先级越高
-            if new_bot_id_index < handle_bot_id_index:
-                handle_bot = bot
-
-        # 没bot连就直接给
-        else:
-            handle_bot = bot
-
-    # 不写就给第一个连的
-    elif not handle_bot:
-        handle_bot = bot
-
-
-# qq机器人断开时执行
-@driver.on_bot_disconnect
-async def on_bot_disconnect(bot: Bot):
-    global handle_bot
-    # 判断掉线的是否为handle bot
-    if bot == handle_bot:
-        # 如果有写bot qq列表
-        if pc.easy_translate_bot_qqnum_list:
-            # 获取当前连着的bot列表(需要bot是在bot qq列表里)
-            available_bot_id_list = [
-                bot_id
-                for bot_id in get_bots()
-                if bot_id in pc.easy_translate_bot_qqnum_list
-            ]
-            if available_bot_id_list:
-                # 打擂台排序？
-                new_bot_index = pc.easy_translate_bot_qqnum_list.index(
-                    available_bot_id_list[0]
-                )
-                for bot_id in available_bot_id_list:
-                    now_bot_index = pc.easy_translate_bot_qqnum_list.index(bot_id)
-                    if now_bot_index < new_bot_index:
-                        new_bot_index = now_bot_index
-                # 取下标在qq列表里最小的bot qq为新的handle bot
-                handle_bot = get_bot(pc.easy_translate_bot_qqnum_list[new_bot_index])
-            else:
-                handle_bot = None
-
-        # 不写就随便给一个连着的(如果有)
-        elif handle_bot:
-            try:
-                new_bot = get_bot()
-                handle_bot = new_bot
-            except ValueError:
-                handle_bot = None
-
 
 async def message_check(event: MessageEvent, bot: Bot) -> bool:
     if isinstance(event, PrivateMessageEvent):
         return True
     elif isinstance(event, GroupMessageEvent):
-        return bot == handle_bot
+        return bot == var.handle_bot
     else:
         return False
 
