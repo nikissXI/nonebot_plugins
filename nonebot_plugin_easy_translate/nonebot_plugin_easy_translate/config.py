e3133cda7bd4f520b4f615cc2ee8a031a6ddcd76
nikissXI <1299577815@qq.com>
2023-01-24 23:19:39 +0800

    0.1.4

--- nonebot_plugin_easy_translate/nonebot_plugin_easy_translate/config.py
+++ nonebot_plugin_easy_translate/nonebot_plugin_easy_translate/config.py
@@ -45,7 +45,7 @@ async def on_bot_connect(bot: Bot):
             var.handle_bot = bot
 
     # 不写就给第一个连的
-    elif not var.handle_bot:
+    elif not pc.easy_translate_bot_qqnum_list and not var.handle_bot:
         var.handle_bot = bot
 
 
